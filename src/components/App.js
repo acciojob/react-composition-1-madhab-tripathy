1133a95d27829b1dc09432bf059e4f6de69486a9
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-08-14 10:23:44 +0000

    app file update

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ import Tabs from "./Tabs";
 const tabs = [
   {
     titles: "Tab 1",
-    contents: "This is the content for Tab 1",
+    contents: "This is the content for Tab 1.",
   },
   {
     titles: "Tab 2",
-    contents: "This is the content for Tab 2",
+    contents: "This is the content for Tab 2.",
   },
   {
     titles: "Tab 3",
-    contents: "This is the content for Tab 3",
+    contents: "This is the content for Tab 3.",
   }
 ]
 const App = () => {
