f08f108d1a842ccdf00d042112fb9cfa837d186d
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-08-14 10:16:22 +0000

    2nd

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ import Tabs from "./Tabs";
 const tabs = [
   {
     title: "Tab 1",
-    content: "This is the content for Tab 1",
+    contents: "This is the content for Tab 1",
   },
   {
     title: "Tab 2",
-    content: "This is the content for Tab 2",
+    contents: "This is the content for Tab 2",
   },
   {
     title: "Tab 3",
-    content: "This is the content for Tab 3",
+    contents: "This is the content for Tab 3",
   }
 ]
 const App = () => {
